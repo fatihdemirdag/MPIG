a22d489c493a7e9bd3a14f90578ed4c9a1226203
fatihdemirdag <fatihdemirdag@hotmail.com>
2023-11-22 01:41:25 +0300

    first release version

--- MainUnit.pas
+++ MainUnit.pas
@@ -6,12 +6,11 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
-  System.Variants,
+  System.Variants, System.IOUtils,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
   FMX.StdCtrls, FMX.Layouts, FMX.ExtCtrls,
   FMX.Controls.Presentation, FMX.TabControl, FMX.Colors, FMX.ListBox, FMX.Edit,
   FMX.EditBox, FMX.SpinBox;
-
 
 
 type
@@ -37,13 +36,21 @@ type
     mmiLoad: TMenuItem;
     mmiExport: TMenuItem;
     mmiQuit: TMenuItem;
+    ProgressBar1: TProgressBar;
+    txtProgress: TLabel;
+    SaveDialog1: TSaveDialog;
     procedure mmiLoadClick(Sender: TObject);
     procedure spnScaleChange(Sender: TObject);
     procedure clrbBackgroundColorChange(Sender: TObject);
     procedure cbxBackgroundTypeChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure mmiQuitClick(Sender: TObject);
+    procedure mmiExportClick(Sender: TObject);
+
   private
     procedure UpdateImage;
+    procedure ExportIconWithSize(bmp: TBitmap; size: integer; fileName: string);
+
   public
     { Public declarations }
   end;
@@ -74,9 +81,78 @@ end;
 
 
 
+procedure TForm1.ExportIconWithSize(bmp: TBitmap; size: integer; fileName: string);
+var
+  tmp: TBitmap;
+begin
+  tmp := TBitmap.Create(size, size);
+
+  tmp.Canvas.BeginScene();
+  tmp.Canvas.DrawBitmap(bmp, bmp.BoundsF, tmp.BoundsF, 1.0, false);
+  tmp.Canvas.EndScene;
+  tmp.SaveToFile(fileName);
+
+  ProgressBar1.Value := ProgressBar1.Value + 1.0;
+end;
+
+
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  imgvFinal.Bitmap := TBitmap.Create(1024, 1024);
+  imgvFinal.Bitmap      := TBitmap.Create(1024, 1024);
+  imgvFinal.BitmapScale := imgvFinal.Width / 1024.0;
+end;
+
+
+
+procedure TForm1.mmiExportClick(Sender: TObject);
+var
+  path: string;
+begin
+  if SaveDialog1.Execute then
+  begin
+    path := ExtractFilePath(SaveDialog1.fileName);
+
+    System.IOUtils.TDirectory.CreateDirectory(path + '\android\');
+    System.IOUtils.TDirectory.CreateDirectory(path + '\ios\');
+    System.IOUtils.TDirectory.CreateDirectory(path + '\ios\iphone\');
+    System.IOUtils.TDirectory.CreateDirectory(path + '\ios\ipad\');
+
+    ProgressBar1.Value := 0;
+    ExportIconWithSize(imgvFinal.Bitmap, 36, path + '\android\launcher_36_ldpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 48, path + '\android\launcher_48_mdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 72, path + '\android\launcher_72_hdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 96, path + '\android\launcher_96_xhdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 144, path + '\android\launcher_144_xxhdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 192, path + '\android\launcher_192_xxxhdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 24, path + '\android\notification_24_mdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 36, path + '\android\notification_36_hdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 48, path + '\android\notification_48_xhdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 72, path + '\android\notification_72_xxhdpi.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 96, path + '\android\notification_96_xxxhdpi.png');
+
+    ExportIconWithSize(imgvFinal.Bitmap, 120, path + '\ios\iphone\ApplicationIcon_120.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 180, path + '\ios\iphone\ApplicationIcon_180.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 1024, path + '\ios\iphone\ApplicationIcon_1024.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 80, path + '\ios\iphone\SpotlightSearchIcon_80.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 120, path + '\ios\iphone\SpotlightSearchIcon_120.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 58, path + '\ios\iphone\SettingIcon_58.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 87, path + '\ios\iphone\SettingIcon_87.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 40, path + '\ios\iphone\NotificationIcon_40.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 60, path + '\ios\iphone\NotificationIcon_60.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 459, path + '\ios\iphone\LaunchImage_2x.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 688, path + '\ios\iphone\LaunchImage_3x.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 459, path + '\ios\iphone\LaunchImage_2x_dark.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 688, path + '\ios\iphone\LaunchImage_3x_dark.png');
+
+    ExportIconWithSize(imgvFinal.Bitmap, 152, path + '\ios\ipad\ApplicationIcon_152.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 167, path + '\ios\ipad\ApplicationIcon_167.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 80, path + '\ios\ipad\SpotlightSearchIcon_80.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 58, path + '\ios\ipad\SettingIcon_58.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 40, path + '\ios\iphone\NotificationIcon_40.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 965, path + '\ios\iphone\LaunchImage.png');
+    ExportIconWithSize(imgvFinal.Bitmap, 965, path + '\ios\iphone\LaunchImage_dark.png');
+  end;
 end;
 
 
@@ -85,16 +161,20 @@ procedure TForm1.mmiLoadClick(Sender: TObject);
 begin
   if OpenDialog1.Execute then
   begin
-    imgvOriginal.Bitmap.LoadFromFile(OpenDialog1.FileName);
+    imgvOriginal.Bitmap.LoadFromFile(OpenDialog1.fileName);
     imgvOriginal.BestFit;
     imgvOriginal.BackgroundFill := TBrush.Create(TBrushKind.None, $00000000);
 
-    lblFileName.Text  := OpenDialog1.FileName;
-    lblImageSize.Text := IntToStr(imgvOriginal.Bitmap.Width) + ' x ' +
-      IntToStr(imgvOriginal.Bitmap.Height);
-
-    imgvFinal.BestFit;
+    lblFileName.Text  := OpenDialog1.fileName;
+    lblImageSize.Text := IntToStr(imgvOriginal.Bitmap.Width) + ' x ' + IntToStr(imgvOriginal.Bitmap.Height);
   end;
+end;
+
+
+
+procedure TForm1.mmiQuitClick(Sender: TObject);
+begin
+  Exit;
 end;
 
 
@@ -107,17 +187,53 @@ end;
 
 
 procedure TForm1.UpdateImage;
+var
+  tmpbmp, tmpmask: TBitmap;
 begin
-  imgvFinal.Bitmap.Canvas.BeginScene();
-  imgvFinal.Bitmap.Canvas.Clear($00000000);
+  tmpbmp  := TBitmap.Create(1024, 1024);
+  tmpmask := TBitmap.Create(1024, 1024);
 
+  tmpbmp.Canvas.BeginScene();
+  tmpbmp.Canvas.Clear($00000000);
+
+  tmpbmp.Canvas.FillRect(TRectF.Create(8.0, 8.0, 1008.0, 1008.0), 1.0,
+    TBrush.Create(TBrushKind.Solid, clrbBackgroundColor.Color));
+
+  tmpbmp.Canvas.DrawBitmap(imgvOriginal.Bitmap, imgvOriginal.Bitmap.BoundsF,
+    TRectF.Create(512.0 - 480.0 * (spnScale.Value / 100.0), 512.0 - 480.0 * (spnScale.Value / 100.0),
+    512.0 + 480.0 * (spnScale.Value / 100.0), 512.0 + 480.0 * (spnScale.Value / 100.0)), 1.0);
+  tmpbmp.Canvas.EndScene;
+
+  // mask
+  tmpmask.Canvas.BeginScene();
+  tmpmask.Canvas.Clear($FF000000);
   if cbxBackgroundType.ItemIndex = 0 then
   begin
-    imgvFinal.Bitmap.Canvas.FillRect(imgvFinal.Bitmap.BoundsF, 1.0,
-      TBrush.Create(TBrushKind.Solid, clrbBackgroundColor.Color));
+    tmpmask.Canvas.FillRect(TRectF.Create(8.0, 8.0, 1008.0, 1008.0), 1.0, TBrush.Create(TBrushKind.Solid, $FFFFFFFF));
+  end
+  else if cbxBackgroundType.ItemIndex = 1 then
+  begin
+    tmpmask.Canvas.FillRect(TRectF.Create(8.0, 8.0, 1008.0, 1008.0), 96.0, 96.0,
+      [TCorner.TopLeft, TCorner.TopRight, TCorner.BottomLeft, TCorner.BottomRight], 1.0,
+      TBrush.Create(TBrushKind.Solid, $FFFFFFFF));
+  end
+  else if cbxBackgroundType.ItemIndex = 2 then
+  begin
+    tmpmask.Canvas.FillRect(TRectF.Create(8.0, 8.0, 1008.0, 1008.0), 192.0, 192.0,
+      [TCorner.TopLeft, TCorner.TopRight, TCorner.BottomLeft, TCorner.BottomRight], 1.0,
+      TBrush.Create(TBrushKind.Solid, $FFFFFFFF));
+  end
+  else if cbxBackgroundType.ItemIndex = 3 then
+  begin
+    tmpmask.Canvas.FillEllipse(TRectF.Create(8.0, 8.0, 1008.0, 1008.0), 1.0,
+      TBrush.Create(TBrushKind.Solid, $FFFFFFFF));
   end;
+  tmpmask.Canvas.EndScene;
 
-  imgvFinal.Bitmap.Canvas.EndScene;
+  if imgvFinal.Bitmap <> nil then
+    imgvFinal.Bitmap := nil;
+
+  imgvFinal.Bitmap := imgvFinal.Bitmap.CreateFromBitmapAndMask(tmpbmp, tmpmask);
 end;
 
 
--- PlaytureMPIG.dpr
+++ PlaytureMPIG.dpr
@@ -3,11 +3,13 @@ program PlaytureMPIG;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  FMX.Skia,
   MainUnit in 'MainUnit.pas' {Form1};
 
 {$R *.res}
 
 begin
+  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
   Application.Run;
